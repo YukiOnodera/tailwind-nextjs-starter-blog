5745e398b8314cdabe37b098c757b7d930831bf3
YukiOnodera <yuuki.seventh.future@gmail.com>
2024-01-07 18:45:12 +0900

    updated log, default posts path

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,8 +8,10 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   // siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  // siteLogo: '/static/images/logo.png',
+  // socialBanner: '/static/images/twitter-card.png',
   siteLogo: '/static/images/logo.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/logo.png',
   github: 'https://github.com/YukiOnodera',
   // mastodon: 'https://mastodon.social/@mastodonuser',
   // email: 'address@yoursite.com',
