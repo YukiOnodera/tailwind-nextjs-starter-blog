21312f0746a2e7c9b8cc06e26a52ee46b33b8304
YukiOnodera <yuuki.seventh.future@gmail.com>
2024-01-25 09:43:51 +0900

    add https://www.googletagmanager.com

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://www.google-analytics.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://www.google-analytics.com googletagmanager.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data: https://www.google-analytics.com;
   media-src *.s3.amazonaws.com;
